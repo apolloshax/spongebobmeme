144b43820140ffa10b5ed9714786144b96b4f5e5
Apollo <bscs18003@itu.edu.pk>
2021-03-22 14:40:17 +0500

    my first commit

---
+++ website.js
@@ -0,0 +1,30 @@
+function replace(str,index,chr)
+{
+	
+		return str.substring(0,index) + chr + str.substring(index+1);
+	
+}
+
+function f1(){
+	
+	var x = document.getElementById("1").value;
+	
+
+
+	for(i=0; i<x.length;i++)
+	{
+		if(i%2 == 0)
+		{
+
+			temp = x.substring(i, i+1);
+			temp = temp.toUpperCase();
+			x = replace(x,i,temp);
+		
+		}
+	}
+
+
+
+document.getElementById("output").innerHTML = "" + x;
+
+}
